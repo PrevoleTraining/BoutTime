af20e32dad886b33aae318505bfea7756d371589
Laurent Prevost <prevole@prevole.ch>
2017-12-14 21:53:57 +0100

    Started to work on main UI screen

--- BoutTime/ViewController.swift
+++ BoutTime/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstEventLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        firstEventLabel.padding = UIEdgeInsets(top: 15, left: 15, bottom: 15, right: 15)
     }
 
     override func didReceiveMemoryWarning() {
